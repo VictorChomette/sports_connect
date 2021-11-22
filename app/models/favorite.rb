fe0ca0e934430567448186579060cd3e69f8474c
Pierre Moulinier <mrmoulinier2440@gmail.com>
2021-11-22 16:46:12 +0100

    validates

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :field
+
+  validates :user, :field, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #un utilisateur peut avoir plusieurs terrains et favoris
+  has_many :favorite_fields, through: :favorites, source: :fields
+  # @user.favorite_fields ==> tous les fields favoris de ton user
 
-
+  validates :age, :favorite_sports, presence: true
+  validates :username, presence: true, uniqueness: true
 end
