089c5c77e85664136c5b5879f98eac0e2f8621db
Victor Chomette <victorchomette@hotmail.fr>
2021-11-22 17:57:54 +0100

    new tables and other stuff

--- app/models/field.rb
+++ app/models/field.rb
@@ -6,20 +6,21 @@ class Field < ApplicationRecord
     outdoor: 1,
     indoor_outdoor: 2
     }, _prefix: true
-    enum status: {
-      open: 0,
-      closed: 1,
-      work_in_progress: 2
-      }, _prefix: true
+  enum status: {
+    open: 0,
+    closed: 1,
+    work_in_progress: 2
+    }, _prefix: true
 
-  #ajout d'une colonne adresse dans laquelle doit etre stoqué l'adresse en question
+  # ajout d'une colonne adresse dans laquelle doit etre stoqué l'adresse en question
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
   # has_many :reviews
   # has_many :presences
   # has_many :favorites
+  has_many :field_sports
 
-  validates :address, :field_types, :status, presence: true
+  validates :address, :status, presence: true
   validates :name, presence: true, uniqueness: true
 end
