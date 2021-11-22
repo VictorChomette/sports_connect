aac78e60ce8618e2cddbd16140384d838f6f7803
Pierre Moulinier <mrmoulinier2440@gmail.com>
2021-11-22 15:15:33 +0100

    geocoder lat long

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_132431) do
+ActiveRecord::Schema.define(version: 2021_11_22_141126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_11_22_132431) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "field_types", default: [], array: true
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,6 +35,9 @@ ActiveRecord::Schema.define(version: 2021_11_22_132431) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.integer "age"
+    t.text "favorite_sports", default: [], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
