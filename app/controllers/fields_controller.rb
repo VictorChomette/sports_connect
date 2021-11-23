7a6395845b3abb992fa919b20a7d67b735cd4187
Pierre Moulinier <mrmoulinier2440@gmail.com>
2021-11-23 15:05:15 +0100

    markers

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -2,25 +2,21 @@ class FieldsController < ApplicationController
   def index
     @fields = Field.all
     #affiche tout les terrains
+    #affiche tout les markers sur la map
+    @markers = @fields.geocoded.map do |field|
+      {
+      lng: field.longitude,
+      lat: field.latitude,
+      info_window: render_to_string(partial: "info_window_fields", locals: { field: field }),
+      }
+    end
   end
 
   def show
-    @fields = Field.find(params[:id])
+    @field = Field.find(params[:id])
     #trouve et affiche un terrain selectionné dans la show avec ses parametres
     authorize @field
-    @markers = [{
-      lng: @field.longitude,
-      lat: @field.latitude,
-      info_window: render_to_string(partial: "info_window_service", locals: { service: @field }),
-      image_url: helpers.asset_url("field.png")
-      },
-        {
-          lng: current_user.longitude,
-          lat: current_user.latitude,
-          info_window: render_to_string(partial: "info_window", locals: { user: current_user }),
-          image_url: helpers.asset_url("user.png")
-        }]
-      #ajout de markers sur la map prenant la long et lat.Une info window rend l'adresse du user
-      # et du terrain.
+    #ajout de markers sur la map prenant la long et lat.Une info window rend l'adresse du user
+    # et du terrain.
   end
 end
