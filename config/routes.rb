e6eeb67a90824958ed39c4e3b4d232d008d98ce6
Jean-Baptiste Guelaud <jbguelaud@gmail.com>
2021-11-23 18:00:14 +0100

    bouton favorie/delete

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "fields", to: "fields#index"
   get "fields/:id", to: "fields#show"
+  resources :fields, only: [:index, :show] do
+    resources :favorites, only: [:create, :destroy]
+  end
 end
