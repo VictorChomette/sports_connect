fa63090b49b0669232ff913b999b391cbb5dd190
Victor Chomette <victorchomette@hotmail.fr>
2021-11-24 14:55:12 +0100

    page my presences created

--- app/controllers/presences_controller.rb
+++ app/controllers/presences_controller.rb
@@ -1,14 +1,13 @@
 class PresencesController < ApplicationController
-  before_action :set_field
+  # before_action :set_field
 
   def index
-    @presences = Presence.all
-    @presences.field = @field
+    @presences = current_user.presences
   end
 
-  private
+  # private
 
-  def set_field
-    @field = Field.find(params[:field_id])
-  end
+  # def set_field
+  #   @field = Field.find(params[:field_id])
+  # end
 end
