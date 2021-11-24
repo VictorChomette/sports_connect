6d5914762d75b2bab1d0e2eb1f3a4f8880c41d52
Pierre Moulinier <mrmoulinier2440@gmail.com>
2021-11-24 11:38:08 +0100

    model et controller de review

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :field
+  validates :comment, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+end
