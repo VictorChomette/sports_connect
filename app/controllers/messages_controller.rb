d20e6f6a6a212b1a22397b7a5be6e0b1ae70d308
Victor Chomette <victorchomette@hotmail.fr>
2021-11-29 16:27:43 +0100

    chatrooms done, but on separate show

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,21 @@
 class MessagesController < ApplicationController
   def create
   @field = Field.find(params[:field_id])
-  @chatroom = Chatroom.find(params[:id])
+  @chatroom = Chatroom.find(params[:chatroom_id])
   @message = Message.new(message_params)
   @message.chatroom = @chatroom
   @message.user = current_user
-  if @message.save
-    redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
-  else
-    render "chatrooms/show"
+    if @message.save
+      ChatroomChannel.broadcast_to(
+      @chatroom,
+      render_to_string(partial: "message", locals: {message: @message})
+      )
+      redirect_to field_chatroom_path(@field, @chatroom, anchor: "message-#{@message.id}")
+    else
+      render "chatrooms/show"
+    end
   end
-  end
+
 
   private
 
