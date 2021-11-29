136b3a27ee9c619afbee9d0c28e1153901ca24c6
Victor Chomette <victorchomette@hotmail.fr>
2021-11-29 17:58:27 +0100

    chatroom on fields page

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,6 +1,13 @@
 import { Controller } from "stimulus";
 import consumer from "../channels/consumer";
 
+const toggleChatBox = () => {
+  console.log("teEEEEEERTYUIOIHGFDCFGHJKVCB");
+  document.querySelector(".chatbox-btn").addEventListener('click', event => {
+    document.querySelector(".chatbox").classList.toggle("d-none");
+  })
+}
+
 export default class extends Controller {
   static values = { chatroomId: Number }
 
@@ -9,6 +16,7 @@ export default class extends Controller {
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
       { received: data => this.element.insertAdjacentHTML("beforeend", data) }
     )
+    toggleChatBox();
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`);
   }
 }
