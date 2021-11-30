fc1c29179c0018579c838dd8d1f2ea734d586c4b
Pierre Moulinier <mrmoulinier2440@gmail.com>
2021-11-30 16:46:00 +0100

    home

--- app/javascript/plugins/typed.js
+++ app/javascript/plugins/typed.js
@@ -3,12 +3,12 @@ import Typed from 'typed.js';
 const typing = () => {
   if (document.querySelector('.element')) {
     const options = {
-      strings: ["Today I'll play <strong>basketball!</strong>",
-      "Today I'll play <strong>tennis!</strong>",
-      "Today I'll <strong>run</strong>",
-      "Today I'll play <strong>football!</strong>"],
-      typeSpeed: 100,
-      backSpeed: 100,
+      strings: ["Today I'll play <strong>Basketball!</strong>",
+      "Today I'll play <strong>Ping-Pong!</strong>",
+      "Today I'll <strong>swim!</strong>",
+      "Today I'll play <strong>Football!</strong>"],
+      typeSpeed: 150,
+      backSpeed: 50,
       smartBackspace: true,
       loop: true,
       showCursor: false
