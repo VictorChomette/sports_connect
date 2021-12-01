dc79518e2b8a6fe7b19e3b74fd8bc019fadaf587
Christophe Potier <potier.christophe@live.fr>
2021-12-01 12:19:55 +0100

    Remise en forme de la page profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_102852) do
+ActiveRecord::Schema.define(version: 2021_12_01_092856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_102852) do
     t.float "longitude"
     t.string "first_name"
     t.string "name"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
